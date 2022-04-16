aae6d16c6dc0d5bdbddd300bec7eba74599c1d39
darcyMaz <92594097+darcyMaz@users.noreply.github.com>
2022-04-16 17:43:24 -0400

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,14 +11,14 @@ function display($path)
 	fclose($file);
 }
 
-if (sizeof($_GET)==0)
-{
+//if (sizeof($_GET)==0)
+//{
 	display("HTDOCS/MATTER/CONTENT/top-of-all-files.txt");
 	display("HTDOCS/MATTER/CONTENT/login-style.txt");
 	display("HTDOCS/MATTER/CONTENT/top-of-all-bodys.txt");
 	display("HTDOCS/MATTER/CONTENT/header.txt");
 	display("HTDOCS/MATTER/CONTENT/login-body.txt");
 	display("HTDOCS/MATTER/CONTENT/bottom-of-all-files.txt");
-}
+//}
 
 ?>
