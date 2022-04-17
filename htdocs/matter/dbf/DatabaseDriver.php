7cfe4ec2e862aa73594abc6cb4e89c64faea9456
Gavin0602 <pdec.prog@gmail.com>
2022-04-17 16:04:43 -0400

    Update errCode_decoder()
    It now returns the error message as string

--- htdocs/matter/dbf/DatabaseDriver.php
+++ htdocs/matter/dbf/DatabaseDriver.php
@@ -48,42 +48,40 @@ class DatabaseDriver
     /** TODO: CHANGE BACK TO SWITCH (MATCH NOT SUPPORTED ON MIMI)
      * Debug tool to decode error code messages
      * @param int $errCode
+     * @return string error message
      */
-    /*
-    function errCode_decoder(int $errCode)
+    function errCode_decoder(int $errCode): string
     {
-        echo match ($errCode)
+        switch ($errCode)
         {
-             0      => "Success",
-            -1      => "Incorrect username length ([1, 30] expected)",
-            -2      => "Username already taken/doesn't exist!",
-            -3      => "Empty password",
-            -4      => "Incorrect password format",
-            -5      => "Incorrect userID format: should be 9 digits",
-            -6      => "userID already taken/doesn't exist!",
-            -7      => "Empty first/last name",
-            -8      => "First/last name too long! (greater than 256 characters)",
-            -9      => "Incorrect email length ([4, 320] expected)",
-            -10     => "Email already taken/doesn't exist!",
-            -11     => "Unknown SQL error or unknown property. (See echo)",
-            -12     => "Invalid query pair",
-            -13     => "Invalid course_title length: ]0, 64] expected!",
-            -14     => "Invalid course_num format: /^[A-Z]{4}[0-9]{3}$/ expected!",
-            -15     => "Invalid course_num format: /^((WINTER)|(SUMMER)|(FALL))[0-9]{4}/ expected!",
-            -16     => "Course not found/already exists!",
-            -17     => "Incorrect date format!",
-            -18     => "User not registered in course!",
-            -19     => "Incorrect rating range : [0, 5] expected!",
-            -20     => "Incorrect message length: should be less than 1024 characters!",
-            -21     => "Incorrect education choice: should be between 'grad', 'ugrad' and 'other'",
-            -22     => "Location too long! Should be less than 256 characters",
-            -23     => "Phone number too long! Should be less than 30 characters",
-            -24     => "Negative degree/hour",
-            -25     => "Note too long! Should be less than 1024 characters",
-            default => "Unknown Error Code!",
-        };
-        echo PHP_EOL;
+            case  0      : return "Success"                                                                   .PHP_EOL;
+            case -1      : return "Incorrect username length ([1, 30] expected)"                              .PHP_EOL;
+            case -2      : return "Username already taken/doesn't exist!"                                     .PHP_EOL;
+            case -3      : return "Empty password"                                                            .PHP_EOL;
+            case -4      : return "Incorrect password format"                                                 .PHP_EOL;
+            case -5      : return "Incorrect userID format: should be 9 digits"                               .PHP_EOL;
+            case -6      : return "userID already taken/doesn't exist!"                                       .PHP_EOL;
+            case -7      : return "Empty first/last name"                                                     .PHP_EOL;
+            case -8      : return "First/last name too long! (greater than 256 characters)"                   .PHP_EOL;
+            case -9      : return "Incorrect email length ([4, 320] expected)"                                .PHP_EOL;
+            case -10     : return "Email already taken/doesn't exist!"                                        .PHP_EOL;
+            case -11     : return "Unknown SQL error or unknown property. (See echo)"                         .PHP_EOL;
+            case -12     : return "Invalid query pair"                                                        .PHP_EOL;
+            case -13     : return "Invalid course_title length: ]0, 64] expected!"                            .PHP_EOL;
+            case -14     : return "Invalid course_num format: /^[A-Z]{4}[0-9]{3}$/ expected!"                 .PHP_EOL;
+            case -15     : return "Invalid course_num format: /^((WINTER)|(SUMMER)|(FALL))[0-9]{4}/ expected!".PHP_EOL;
+            case -16     : return "Course not found/already exists!"                                          .PHP_EOL;
+            case -17     : return "Incorrect date format!"                                                    .PHP_EOL;
+            case -18     : return "User not registered in course!"                                            .PHP_EOL;
+            case -19     : return "Incorrect rating range : [0, 5] expected!"                                 .PHP_EOL;
+            case -20     : return "Incorrect message length: should be less than 1024 characters!"            .PHP_EOL;
+            case -21     : return "Incorrect education choice: should be between 'grad', 'ugrad' and 'other'" .PHP_EOL;
+            case -22     : return "Location too long! Should be less than 256 characters"                     .PHP_EOL;
+            case -23     : return "Phone number too long! Should be less than 30 characters"                  .PHP_EOL;
+            case -24     : return "Negative degree/hour"                                                      .PHP_EOL;
+            case -25     : return "Note too long! Should be less than 1024 characters"                        .PHP_EOL;
+            default      : return "Unknown Error Code!"                                                       .PHP_EOL;
+        }
     }
-    */
 
 }
